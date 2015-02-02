19bb26f541ab4dc1e818d0492a6792e33cd717f9
aaron <joeyanhhuynh@yahoo.ca>
2015-02-02 01:44:44 -0500

    style and media query change media query for desktop and tablet

--- js/push-menu.js
+++ js/push-menu.js
@@ -1,5 +1,4 @@
 // Needs latest version of jQuery to run
-
 $(function() {
   $('body').removeClass('noscript');
 
@@ -12,8 +11,11 @@ function toggleNav() {
   if ($('.site-wrapper').attr('data-state') == 'slide-closed') {
     // Display Nav when closed
     $('.site-wrapper').attr('data-state', 'slide-open');
+    $('.close-btn').removeClass('invis');
   } else {
     // Hide Nav when open
     $('.site-wrapper').attr('data-state', 'slide-closed');
+    $('.close-btn').addClass('invis');
+    //$('.toggle-btn').show();
   }
 }
